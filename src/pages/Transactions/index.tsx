ddaa485667193c19d614fb938ec298fdeaf70cd3
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-03-13 16:48:06 -0300

    Configurado cards e header[

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary/summary";
+
+export function Transactions() {
+    return(
+        <div>
+            <Header />
+            <Summary />
+        </div>
+    )
+}
