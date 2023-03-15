8158775bd7f56c2a7f962f6e434faf3e350cd938
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-03-15 11:10:27 -0300

    Configurado estilização dos botoes de entrada e saida

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from '@radix-ui/react-radio-group';
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
 
@@ -19,11 +20,11 @@ export function NewTransactionModal() {
                     <input type='text' placeholder="Categoria" required />
 
                     <TransactionType>
-                        <TransactionTypeButton variant="income">
+                        <TransactionTypeButton variant="income" value="income">
                             <ArrowCircleUp />
                             Entrada
                         </TransactionTypeButton>
-                        <TransactionTypeButton variant="outcome">
+                        <TransactionTypeButton variant="outcome" value="outcome">
                             <ArrowCircleDown />
                             Saída
                         </TransactionTypeButton>
