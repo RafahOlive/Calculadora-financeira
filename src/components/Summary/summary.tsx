3c1859969f508086bee39da810f6bf5223a82005
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-03-16 17:32:11 -0300

    Configurado logica de soma de entrada e saida

--- src/components/Summary/summary.tsx
+++ src/components/Summary/summary.tsx
@@ -5,30 +5,50 @@ import { TransactionsContext } from "../../contexts/TransactionContext";
 
 export function Summary() {
     const { transactions } = useContext(TransactionsContext)
-    return(
+
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+            if(transaction.type === 'income') {
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            }else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+
+            return acc;
+         },
+        {
+            income: 0,
+            outcome: 0,
+            total: 0
+        }
+    )
+
+    return (
         <SummaryContainer>
             <SummaryCard>
                 <header>
                     <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e"/>
+                    <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
-                    <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
                 <header>
                     <span>Saidas</span>
-                    <ArrowCircleDown size={32} color="#f75a68"/>
+                    <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
-                    <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
-                    <CurrencyDollar size={32} color="#fff"/>
+                    <CurrencyDollar size={32} color="#fff" />
                 </header>
-                    <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
