1b8d7acf312381d209fdb3d9c6c30f63181a53dc
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-03-16 10:45:09 -0300

    Criado primeira req jsonserver

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,42 +1,56 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary/summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TranactionsContainer, TransactionsTable } from "./styles";
 
+interface Transactions {
+    id: number,
+    description: string;
+    type: 'income' | 'outcome';
+    price: number,
+    category: string,
+    createdAt: string
+}
+
 export function Transactions() {
-    return(
+    const [transactions, setTransactions] = useState<Transactions[]>([])
+
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3000/transactions')
+        const data = await response.json();
+        setTransactions(data)
+    }
+    useEffect(() => {
+        loadTransactions();
+    }, [])
+
+    return (
         <div>
             <Header />
             <Summary />
 
-           <TranactionsContainer>
-            <SearchForm />
-           <TransactionsTable>
-                <tbody>
-                    <tr>
-                        <td width='40%'>Desenvolvimento de site</td>
-                        <td>
-                            <PriceHighlight variant="income">
-                                R$ 12.000,00
-                            </PriceHighlight>
-                        </td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-
-                    <tr>
-                        <td width='40%'>Hamburguer</td>
-                        <td>
-                        <PriceHighlight variant="outcome">
-                            - R$ 12,00
-                        </PriceHighlight> 
-                        </td>
-                        <td>Alimentação</td>
-                        <td>10/04/2022</td>
-                    </tr>
-                </tbody>
-            </TransactionsTable>
-           </TranactionsContainer>
+            <TranactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width='40%'>{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </TransactionsTable>
+            </TranactionsContainer>
         </div>
     )
 }
